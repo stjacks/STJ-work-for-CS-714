790531ac9c00f74ebe2a33f0cb97dabf75de631e
Samuel Jackson <stjackson@cs.wisc.edu>
2020-11-14 10:48:03 -0600

    bug fixes

--- homework3/Nabla.m
+++ homework3/Nabla.m
@@ -16,20 +16,20 @@ uys = zeros(N+1,N+1);
 
 for i = 1:N+1
     v = vv(i,:);
-    w = chebfft(v);
+    w = chebfft(v');
     for j=2:numPartials
         w = chebfft(w);
     end
-    uxs(i,:) = w;
+    uxs(i,:) = w';
 end
 
 for i = 1:N+1
     v = vv(:,i);
-    w = chebfft(v');
+    w = chebfft(v);
     for j=1:numPartials
         w = chebfft(w);
     end
-    uys(:,i) = w';
+    uys(:,i) = w;
 end
 
 uTot = uxs + uys;
--- homework3/testDifferentiation.m
+++ homework3/testDifferentiation.m
@@ -2,6 +2,10 @@ N = 10;
 [D,x] = cheb(N);
 u = x; % linear function
 
+w = chebfft(u) % should be all ones, subject to numerical error
+v = chebfft(w) % should be all zeros, subject to numerical error
+
+% U(x,y) = x + y
 U = zeros(N+1,N+1);
 for i = 1:N+1
     for j = 1:N+1
@@ -10,3 +14,7 @@ for i = 1:N+1
 end
 
 Nabla(U,1)
+% should be a matrix of all ones, subject to numerical error
+
+Nabla(U,2)
+% should be a matrix of all zeros, subject to numerical error
