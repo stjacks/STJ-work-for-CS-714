7d05d37a79bc8719857077bae954850512e69d30
Samuel Jackson <stjackson@cs.wisc.edu>
2020-11-15 19:16:03 -0600

    bug fixes

--- homework3/ProblemCc.m
+++ homework3/ProblemCc.m
@@ -4,14 +4,12 @@ clear;
 N = 100;
 thetas = (1/N)*pi*(0:N);
 xs = cos(thetas); % these values go from -1 to 1. We need from 0 to 1
-xs = (1/2)*xs + 0.5;
 ys = xs';
 dt = 6*(1/N)^2; % smaller than the CFL condition
 
 fN = 600; % fine grid
 fthetas = (1/fN)*pi*(0:fN);
 fxs = cos(fthetas); % these values go from -1 to 1. We need from 0 to 1
-fxs = (1/2)*fxs + 0.5;
 fys = fxs';
 fdt = 6*(1/fN)^2; % smaller than the CFL condition
 
@@ -28,10 +26,11 @@ for tmp=2:fineRounds
    [fcurr, fold] = step(fcurr, fold, fdt);
 end
 
-numRounds = round(0.1/dt);
+numRounds = round(0.5/dt);
 
 error = max(0,maxError(fcurr, vcurr, fN / N));
 disp(numRounds)
+
 for rounds=2:numRounds
 
     if mod(rounds, 10) == 0
@@ -39,7 +38,7 @@ for rounds=2:numRounds
         disp(['Current error: ', num2str(error)]);
     end
     [vcurr, vold] = step(vcurr, vold, dt);
-    
+
     for tmp=1:fineRounds
        %if mod(tmp, 10) == 0
        %    disp(['findIteration ', num2str(tmp), ' out of ', num2str(fineRounds)]);
@@ -51,5 +50,6 @@ for rounds=2:numRounds
     error = max(error,maxError(fcurr, vcurr, fN / N));
     
 end
+
 disp(error);
 disp(['Error for ', num2str(N), ' is ', num2str(error)]);
